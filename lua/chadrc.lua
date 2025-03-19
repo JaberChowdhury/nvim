70da1989efccba749fe9fe61777a5389ff7baf0b
Black Light <78747748+JaberChowdhury@users.noreply.github.com>
2025-03-20 02:46:45 +0600

    Remove LICENSE file and update Neovim configuration for improved plugin management and formatting

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,8 +6,8 @@
 local M = {}
 
 M.base46 = {
-	theme = "nord",
-
+	theme = "catppuccin",
+    transparency = true,
 	hl_override = {
 		Comment = { italic = true },
 		["@comment"] = { italic = true },
@@ -18,7 +18,22 @@ M.nvdash = { load_on_startup = true }
 M.ui = {
       tabufline = {
          lazyload = false
-     }
+	} 
 }
-
+M.statusline = {
+      enabled = true,
+      theme = "default", -- default/vscode/vscode_colored/minimal
+      -- default/round/block/arrow separators work only for default statusline theme
+      -- round and block will work for minimal theme only
+      separator_style = "default",
+      order = nil,
+      modules = nil,
+}
+M.tabufline = {
+      enabled = true,
+      lazyload = true,
+      order = { "treeOffset", "buffers", "tabs", "btns" },
+      modules = nil,
+      bufwidth = 21,
+}
 return M
